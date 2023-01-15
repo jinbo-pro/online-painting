5db97fd257767d7f12b6ffe4cc559722bc47340a
lijinbo-LX <lijinbode@foxmail.com>
2023-01-15 18:56:51 +0800

    进度条

---
+++ src/components/ProgressStep.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="step_max">
+    <div class="step_inner_bgc" :style="{ width: stepBgcLang }"></div>
+    <div class="bar_max jsb ac">
+      <div v-for="(item, index) in stepCount" :key="item" class="bar_box">
+        <div :class="['bar', { active_bar: (progress / 100) * (stepCount - 1) >= index }]"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressStep',
+  props: {
+    progress: {
+      type: Number,
+      default: 0
+    },
+    stepCount: {
+      type: Number,
+      default: 5
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    stepBgcLang() {
+      if (!this.progress || this.progress < 0) return 0
+      if (this.progress >= 100) return '100%'
+      return this.progress + '%'
+    }
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+$box-h: 24px;
+$bar-size: 40px;
+
+.step_max {
+  width: 100%;
+  height: $box-h;
+  border: 1px solid #f1f1f1;
+  box-sizing: border-box;
+  box-shadow: 0px 1px 1px rgba(125, 164, 83, 0.3), inset 0px 1px 1px rgba(48, 48, 48, 0.1);
+  position: relative;
+  .step_inner_bgc {
+    top: 0;
+    left: 0;
+    z-index: 100;
+    position: absolute;
+    width: 0;
+    height: $box-h;
+    background-color: #7da453;
+  }
+  .bar_max {
+    width: calc(100% + $bar-size);
+    position: absolute;
+    top: 0;
+    left: -($bar-size / 2);
+    z-index: 200;
+    transform: translateY(-25%);
+  }
+  .bar_box {
+    width: $bar-size;
+    height: $bar-size;
+    &:hover {
+      .bar {
+        animation: barAnimation 0.3s ease-in-out;
+      }
+    }
+  }
+  .bar {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #fff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+  .active_bar {
+    background-color: #7da453;
+  }
+}
+@keyframes barAnimation {
+  0% {
+    transform: scale(0.9);
+  }
+
+  80% {
+    transform: scale(1.1);
+  }
+
+  to {
+    transform: scale(1);
+  }
+}
+</style>
--- src/pages/reflect/selectReflectionMode.vue
+++ src/pages/reflect/selectReflectionMode.vue
@@ -5,15 +5,8 @@
       <el-image class="left_cover" src="http://www.ruanyifeng.com/images_pub/pub_1.jpg"></el-image>
       <div class="ml-16">
         <p>Weekly progress</p>
-        <div class="step_max">
-          <div class="step_inner_bgc" :style="{ width: stepBgcLang }"></div>
-          <div class="bar_max jsb ac">
-            <div
-              v-for="(item, index) in barList"
-              :key="item.id"
-              :class="['bar', { active_bar: activeStep > index }]"
-            ></div>
-          </div>
+        <div class="progress_max pl-24">
+          <ProgressStep :progress="progress" :stepCount="7" />
         </div>
       </div>
     </div>
@@ -21,32 +14,17 @@
 </template>
 
 <script>
-import PaintingItem from '@/components/Painting/PaintingItem.vue'
-import { listData } from '@/utils/mock'
+import ProgressStep from '@/components/ProgressStep.vue'
 export default {
   components: {
-    PaintingItem
+    ProgressStep
   },
   data() {
     return {
-      isDone: 1,
-      radio: 2,
-      barList: [],
-      formData: {
-        emoji: '',
-        notes: ''
-      },
-      activeStep: 3
+      progress: 25
     }
   },
-  computed: {
-    stepBgcLang() {
-      return (600 / 7) * this.activeStep + 'px'
-    }
-  },
-  created() {
-    this.barList = listData(7)
-  },
+  created() {},
   methods: {}
 }
 </script>
@@ -57,40 +35,7 @@ export default {
   height: 227px;
   border-radius: 20px;
 }
-
-.step_max {
-  width: 600px;
-  height: 24px;
-  border: 1px solid #e1e1e1;
-  box-sizing: border-box;
-  box-shadow: 0px 1px 1px rgba(125, 164, 83, 0.3), inset 0px 1px 1px rgba(48, 48, 48, 0.1);
-  position: relative;
-  .step_inner_bgc {
-    top: 0;
-    left: 0;
-    z-index: 10;
-    position: absolute;
-    height: 24px;
-    width: 0;
-    background-color: #7da453;
-  }
-  .bar_max {
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 20;
-  }
-  .bar {
-    width: 44px;
-    height: 44px;
-    border-radius: 50%;
-    background-color: #fff;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    transform: translateY(-25%);
-  }
-  .active_bar {
-    background-color: #7da453;
-  }
+.progress_max {
+  width: 500px;
 }
 </style>
